c72e23d37562cdcd6c9bb1e3b999775f413d3324
María Vedia <maria.vedia@kairosds.com>
2021-07-07 20:47:06 +0200

    feat: apply i18n to form

--- src/views/Form/index.js
+++ src/views/Form/index.js
@@ -3,10 +3,12 @@ import Input from '../../components/Input'
 import Button from '../../components/Button'
 import { isValidPassword } from './validation'
 import './Form.css';
+import { useTranslation } from 'react-i18next'
 
 const Step2 = ({ step, setStep, password, setPassword }) => {
-    const [errors, setErrors] = useState({})
- const handleSetPassword = ({ target }) => {
+  const { t } = useTranslation()
+  const [errors, setErrors] = useState({})
+  const handleSetPassword = ({ target }) => {
     setPassword({
       type: 'SET_PASSWORD',
       payload: { [target.name]: target.value },
@@ -22,52 +24,60 @@ const Step2 = ({ step, setStep, password, setPassword }) => {
   }
   return (
     <>
-        <h2 className="form__text-initial">En primer lugar, debes crear una contraseña diferente para tus pertenencias electrónicas. No podrás recuperar tu contraseña, así que recuérdala bien.</h2>
-        <form onSubmit={handleNext}>
+      <h2 className="form__text-initial">{t('form.text1')}</h2>
+
+      <form onSubmit={handleNext}>
         <div className="form">
           <div className="form__group">
-          <Input
-            type="password"
-            name="password1"
-            value={password.password1}
-            label="Crea tu contraseña maestra"
-            placeholder="Teclea tu contraseña"
-            onChange={(e) => handleSetPassword(e)}
-            error={errors.password1}
-          />
-          <Input
-            type="password"
-            name="password2"
-            value={password.password2}
-            label="Repite tu contraseña maestra"
-            placeholder="Repite tu contraseña"
-            onChange={(e) => handleSetPassword(e)}
-            error={errors.password2}
-          />
+            <Input
+              type="password"
+              name="password1"
+              value={password.password1}
+              label={t('form.inputPassword.label')}
+              placeholder={t('form.inputPassword.placeholder')}
+              onChange={(e) => handleSetPassword(e)}
+              error={errors.password1}
+            />
+            <Input
+              type="password"
+              name="password2"
+              value={password.password2}
+              label={t('form.inputPassword2.label')}
+              placeholder={t('form.inputPassword2.placeholder')}
+              onChange={(e) => handleSetPassword(e)}
+              error={errors.password2}
+            />
           </div>
           <div>
-            <h2 className="form__text">También puedes crear una pista que te ayude a recordar tu contraseña maestra</h2>
+            <h2 className="form__text">{t('form.text2')}</h2>
             <div className="form__clue">
               <Input
                 type="text"
                 name="clue"
                 value={password.clue}
-                label="Crea tu pista para recordar tu contraseña (Opcional)"
-                placeholder="Introduce tu pista"
+                label={t('form.inputClue.label')}
+                placeholder={t('form.inputClue.placeholder')}
                 onChange={(e) => handleSetPassword(e)}
                 error={errors.clue}
             />
-            <span className="input__total-value">{`${password.clue.length}/60`}</span>
-           </div>
+              <span className="input__total-value">{`${password.clue.length}/60`}</span>
+            </div>
+            </div>
           </div>
-          </div>
-          <div className="form__buttons">
-            <Button onClick={() => setStep(step - 1)} type="button">
-              Cancelar
-            </Button>
-            <Button type="submit"> Siguiente</Button>
-          </div>
-        </form>
+        <div className="form__buttons">
+          <Button
+            onClick={() => setStep(step - 1)}
+            type="button"
+          >
+            {t('button.cancel')}
+          </Button>
+          <Button
+            type="submit"
+          >
+            {t('button.next')}
+          </Button>
+        </div>
+      </form>
     </>
   )
 }
